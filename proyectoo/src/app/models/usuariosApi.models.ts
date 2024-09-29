bfa54ac939aca255839a1df3f206bd58c47b8527
MauricioJeria <mau.jeria@duocuc.cl>
2024-09-29 20:42:27 -0300

    actualizacion login

---
+++ proyectoo/src/app/models/usuariosApi.models.ts
@@ -0,0 +1,8 @@
+export interface UsuarioAPI { // Definir la interface para los usuarios de la API
+  id: string,
+  usuario: string,
+  clave: string,
+  nombre: string,
+  phone: string,
+  rol: string
+}
